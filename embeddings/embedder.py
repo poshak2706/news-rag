1611de26ef26881f8f7d1d75343097402eb8b1c8
Poshak Arora <poshak3367@gmail.com>
2026-04-08 03:12:37 +0530

    fixed embedder

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -2,9 +2,16 @@ from google import genai
 from google.genai import types
 import os
 
-client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+def get_client():
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not api_key:
+        raise ValueError("API key missing")
+
+    return genai.Client(api_key=api_key)
 
 def get_embeddings(texts):
+    client=get_client()
     response = client.models.embed_content(
         model="gemini-embedding-001",
         contents=texts,
